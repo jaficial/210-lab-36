80bf91071417c6cb507d4413614d8956246a52c9
Jed Aficial's PC <jedaficial1@gmail.com>
2024-12-01 17:53:27 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -8,18 +8,21 @@ int main() {
     // then write driver file in main to populate a BST with dataset file 
     // add interactive menu-driven functionality to add, delete, search, and modify records
     IntBinaryTree tree;
-
+    
     cout << "Inserting nodes. ";
     string temp = "test1";
     string temp2 = "test2";
     tree.insertNode(temp);
     tree.insertNode(temp2);
-    // tree.insertNode(3);
-    // tree.insertNode(12);
-    // tree.insertNode(9);
-    
     cout << "Done.\n";
     tree.displayPreOrder();
+    tree.remove(temp2);
+    cout << "After removing test2 node: " << endl;
+    tree.displayPreOrder();
+    tree.insertNode(temp2);
+    cout << "Added test2 back in: " << endl;
+    tree.searchNode(temp2);
+    
 
     return 0;
 }
